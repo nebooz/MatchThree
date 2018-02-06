a0c032c0ae619565d9864c86597a9ac89f7049aa
Maximiliano Diaz <nebooz@gmail.com>
2018-02-05 23:35:46 -0800

    End of day 1 of the M3 Tutorial

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    public int width;
+    public int height;
+
+    public int borderSize;
+
+    public GameObject tilePrefab;
+
+    Tile[,] m_allTiles; //2-dimensional array
+
+	void Start () {
+        m_allTiles = new Tile[width, height];
+        SetupTiles();
+        SetupCamera();
+	}
+
+    void SetupTiles()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject; //as GameObject is to cast it as a GameObject on Instantiate
+
+                tile.name = "Tile (" + i + "," + j + ")";
+
+                m_allTiles[i, j] = tile.GetComponent<Tile>();
+
+                tile.transform.parent = transform;
+            }
+        }
+    }
+
+    void SetupCamera()
+    {
+        Camera.main.transform.position = new Vector3((float)(width - 1) / 2f, (float)(height - 1) / 2f, -10f);
+
+        float aspectRatio = (float)Screen.width / (float)Screen.height;
+
+        float verticalSize = (float)height / 2f + (float)borderSize;
+
+        float horizontalSize = ((float)width / 2f + (float)borderSize) / aspectRatio;
+
+        Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize; //whoever is larger wins
+
+
+
+    }
+	
+}
